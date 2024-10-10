3236287a67c1354d171866f898b7e6062dd8e5c1
RanP90 <122150955+RanP90@users.noreply.github.com>
2024-10-10 11:28:10 -0700

    FIxed ALLOWED_ORIGIN url

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const url = require('url');
 const path = require('path');
 const { queryAsync } = require('./db');
 const { ServerError, InvalidBody, InvalidQuery, InvalidQueryType, notFound } = require('./lang/en/en'); // Removed unused insertSuccess and insertJSONError
-const ALLOWED_ORIGIN = 'https://comp4537lab04server1.netlify.app';
+const ALLOWED_ORIGIN = 'https://comp4537lab5server1.netlify.app';
 
 // Helper function to start the server
 const startServer = (port, requestHandler) => {
@@ -98,7 +98,7 @@ const requestHandler = (req, res) => {
     const path = parsedUrl.pathname;
 
     // CORS Headers
-    res.setHeader('Access-Control-Allow-Origin', ALLOWED_ORIGIN); // Corrected here
+    res.setHeader('Access-Control-Allow-Origin', ALLOWED_ORIGIN); 
     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
